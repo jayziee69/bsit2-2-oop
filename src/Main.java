6933295b3a19b495c338350257838b6478aa4e34
jayziee69 <rjamparo61386@liceo.edu.ph>
2025-08-18 11:51:28 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GradeCalculator calculator = new GradeCalculator();
+
+        String studentName = "John Smith";
+        double average = calculator.calculateAverage(85.5, 92.0, 78.5, 90.0);
+        String letterGrade = calculator.getLetterGrade(average);
+
+        System.out.println();
+        calculator.displayResult(studentName, average);
+
+        System.out.println();
+        calculator.displayResult(studentName, average, letterGrade);
     }
 }
